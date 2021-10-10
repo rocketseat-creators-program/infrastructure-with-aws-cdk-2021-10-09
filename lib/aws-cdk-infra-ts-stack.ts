7af723a3861f6d362205c68bebcdad506665e350
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2021-10-09 21:29:20 -0300

    feat: create s3 bucket

--- lib/aws-cdk-infra-ts-stack.ts
+++ lib/aws-cdk-infra-ts-stack.ts
@@ -1,9 +1,13 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
 
 export class AwsCdkInfraTsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new s3.Bucket(this, 'ExpertsClubS3', {
+      versioned: true
+    })
+    
   }
 }
