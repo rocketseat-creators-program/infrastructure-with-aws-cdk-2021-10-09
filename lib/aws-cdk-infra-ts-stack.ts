7678808bf020a2003927d1b2fe4e40d9416b58a2
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2021-10-09 21:35:09 -0300

    feat: create rest API gateway

--- lib/aws-cdk-infra-ts-stack.ts
+++ lib/aws-cdk-infra-ts-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
 
 export class AwsCdkInfraTsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -14,6 +15,10 @@ export class AwsCdkInfraTsStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'index.handler',
+    })
+    
+    new apigw.LambdaRestApi(this, 'ExpertsClubAPI-GW', {
+      handler: lambdaExperts
     })
   }
 }
