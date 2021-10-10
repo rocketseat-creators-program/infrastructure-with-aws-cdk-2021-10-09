997c28a55fc6f0833abbcaced23020773cc322c1
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2021-10-09 21:36:02 -0300

    feat: create DynamoDB table

--- lambda/index.js
+++ lambda/index.js
@@ -1,6 +1,17 @@
+const { DynamoDB } = require('aws-sdk');
+
 exports.handler = async function (event) {
+  const dynamo = new DynamoDB();
 
   console.log("request:", JSON.stringify(event, undefined, 2));
+
+  // update dynamo entry for "path" with hits++
+  await dynamo.updateItem({
+    TableName: process.env.TABLE_NAME,
+    Key: { path: { S: event.path } },
+    UpdateExpression: 'ADD hits :incr',
+    ExpressionAttributeValues: { ':incr': { N: '1' } }
+  }).promise();
 
   return {
     statusCode: 200,
--- lib/aws-cdk-infra-ts-stack.ts
+++ lib/aws-cdk-infra-ts-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
+import * as dynamo from '@aws-cdk/aws-dynamodb';
 
 export class AwsCdkInfraTsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -11,11 +12,23 @@ export class AwsCdkInfraTsStack extends cdk.Stack {
       versioned: true
     })
     
+    const dynamoTable = new dynamo.Table(this, 'ExpertsClub-Dynamo', {
+      partitionKey: {
+        name: "path",
+        type: dynamo.AttributeType.STRING
+      }
+    })
+
     const lambdaExperts = new lambda.Function(this, 'ExpertsClubLambda', {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'index.handler',
+      environment: {
+        TABLE_NAME: dynamoTable.tableName
+      }
     })
+
+    dynamoTable.grantReadWriteData(lambdaExperts)
     
     new apigw.LambdaRestApi(this, 'ExpertsClubAPI-GW', {
       handler: lambdaExperts
